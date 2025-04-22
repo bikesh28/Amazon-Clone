0732518c5640b8d4e898b17308b921da162c84c3
bikesh28 <bikeshsapkota12@gmail.com>
2025-04-22 15:16:14 +0545

    Cart and Cart Quantity

---
+++ Amazon/data/cart.js
@@ -0,0 +1,4 @@
+window.cart = [];
+
+ console.log("cart is initialized");
+
--- Amazon/script/amazon.js
+++ Amazon/script/amazon.js
@@ -44,12 +44,14 @@ products.forEach((product)=>{
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart ">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id= "${product.id}"
+            >
                 Add to Cart
             </button>
         </div>
@@ -58,3 +60,33 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').
     innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click',() => {
+            const productId = button.dataset.productId;
+            let matchingItem;
+            
+            cart.forEach((item)=>{
+                if(productId === item.productId){
+                    matchingItem = item;
+                }
+            });
+
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }else{
+                window.cart.push({
+                    productId : productId,
+                    quantity: 1
+                });
+            }
+
+            cartQunatity = 0;
+            cart.forEach((item)=>{
+                cartQunatity =+ item.quantity;
+            });
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQunatity;
+        });
+});
