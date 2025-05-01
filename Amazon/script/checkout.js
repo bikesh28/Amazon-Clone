8700ad7016c76be7fcffbdd84c3a0234d02dc952
bikesh28 <bikeshsapkota12@gmail.com>
2025-05-01 15:16:11 +0545

    making delivery options interactive

--- Amazon/script/checkout.js
+++ Amazon/script/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, updateCartQuantity} from '../data/cart.js';
+import {cart, removeFromCart, updateCartQuantity, updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -97,7 +97,9 @@ function deliveryOptionHTML(matchingProduct, cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOption.id;
         html +=
         `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option"
+             data-product-id="${matchingProduct.id}"
+             data-delivery-option-id="${matchingProduct.deliveryOptionId}">
                 <input type="radio"
                   ${isChecked ? 'checked' : ''}
                  class="delivery-option-input"
@@ -169,6 +171,16 @@ document.querySelectorAll('.js-update-link')
             updatingItem.classList.add('is-editing-quantity');
         })
     });
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () =>{
+            const{productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId)
+        })
+    })
+
+
 
 // document.querySelectorAll('.save-quantity-link')
 //     .forEach((saveLink)=> {
