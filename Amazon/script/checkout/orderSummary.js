ef114dc1ce200a5ee93188755d988bfa82a78ce2
bikesh28 <bikeshsapkota12@gmail.com>
2025-05-02 19:00:46 +0545

    save data , generate html for payment summary

--- Amazon/script/checkout/orderSummary.js
+++ Amazon/script/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import {cart, removeFromCart, updateCartQuantity, updateDeliveryOption} from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products , getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryoptions.js';
+import { deliveryOptions, getDeliveryOption} from '../../data/deliveryoptions.js';
 
 const today = dayjs();
 const deliveyDateSeven = today.add(7, 'days');
@@ -13,28 +13,17 @@ console.log(deliveyDateThree);
 
 export function renderOrderSummary(){
 
-
-
     let cartSummaryHTML='';
 
     cart.forEach((cartItem) => {
         const productId = cartItem.productId;
 
-        let matchingProduct; 
-        products.forEach((product) =>{
-            if (product.id === productId){
-                matchingProduct = product;
-            }
-        });
+        const matchingProduct = getProduct(productId);
 
         const deliveryOptionId = cartItem.deliveryOptionId;
 
-        let deliveryOption;
-        deliveryOptions.forEach((option) => {
-            if(option.id === deliveryOptionId){
-                deliveryOption = option;
-            }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
+
         const today = dayjs();
             const deliveryDate = today.add(
                 deliveryOption.deliveryDays,'days'
@@ -98,7 +87,7 @@ export function renderOrderSummary(){
             const dateString = deliveryDate.format('dddd, MMMM D');
 
             const priceString = (deliveryOption.priceCents) === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`
-            const isChecked = deliveryOption.id === cartItem.deliveryOption.id;
+            const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
             html +=
             `
                 <div class="delivery-option js-delivery-option"
