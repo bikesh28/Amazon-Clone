fa7662acf6965897567570b05438bfd22c5aa65e
bikesh28 <bikeshsapkota12@gmail.com>
2025-05-03 21:29:18 +0545

    rc

--- Amazon/test/testMoney.js
+++ Amazon/test/testMoney.js
@@ -18,10 +18,3 @@ if(formatCurrency(0) === '0.00'){
 }else{
     console.log("failed");
 }
-
-console.log('ROunds up to nearest cent');
-if(formatCurrency(2000.5) === '20.01'){
-    console.log("passed");
-}else{
-    console.log("failed");
-}
