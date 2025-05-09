b011abbdcfc09d32aa3461e229e52d0b235ce40d
bikesh28 <bikeshsapkota12@gmail.com>
2025-05-09 16:19:12 +0545

    converting code to Promises

--- Amazon/script/checkout.js
+++ Amazon/script/checkout.js
@@ -1,19 +1,62 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 //import '../data/backend-practice.js';
 // import '../data/cart-oop.js';
 
+
+Promise.all([
+    new Promise((resolve) =>{
+        loadProducts(() =>{
+            resolve();
+        });
+    }),
+
+    new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
+
+        });
+    })
+]).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary(); 
+});
+
+
+
+/*
+
 new Promise((resolve) =>{
     loadProducts(() =>{
         resolve();
-    })
+    });
+
+
 }).then(() =>{
+    return new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
 
-});
+        });
+    }); 
 
-loadProducts(() =>{
+
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
+
+/*
+
+loadProducts(() =>{
+    loadCart(() =>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+
+*/
